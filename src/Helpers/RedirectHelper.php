a46a33261e89b85891bb6db6a9ecc745616bea59
Stepan <pirogov1923@mail.ru>
2023-10-27 17:23:41 +0500

    чистка main ветки

--- src/Helpers/RedirectHelper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Src\Helpers;
-
-class RedirectHelper
-{
-
-}
--- src/Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Src;
-require_once __DIR__ . '/../vendor/autoload.php';
-session_start();
-
-unset($_SESSION['phone']);
-unset($_SESSION['name']);
-Header("Location: ../index.php");
