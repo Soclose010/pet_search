22edc77741440e54e23f70a3d46419e026698966
Stepan <pirogov1923@mail.ru>
2023-10-31 17:53:47 +0500

    сделал заготовку для работы с базой

---
+++ app/classes/Db.php
@@ -0,0 +1,48 @@
+<?php
+
+class Db
+{
+    private PDO $connect;
+    private PDOStatement $stmt;
+    private static ?Db $instance = null;
+
+    public static function getInstance(): Db
+    {
+        if (is_null(self::$instance))
+            self::$instance = new self();
+        return self::$instance;
+    }
+
+    public function connection(array $db_config): static
+    {
+        $dsn = "mysql:host={$db_config['host']};dbname={$db_config['dbname']};port={$db_config['port']}";
+        try {
+            $this->connect = new PDO($dsn, $db_config['username'], $db_config['password'], $db_config['options']);
+        } catch (PDOException) {
+            abort(500);
+        }
+        return $this;
+    }
+
+    public function rawSql(string $query, array $params = []): bool|array
+    {
+        $this->stmt = $this->connect->prepare($query);
+        $this->stmt->execute($params);
+        return $this->stmt->fetchAll();
+    }
+
+    private function __clone(): void
+    {
+
+    }
+
+    private function __construct()
+    {
+    }
+
+    public function __wakeup(): void
+    {
+
+    }
+
+}
