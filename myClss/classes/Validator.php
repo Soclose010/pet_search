d13f41fca05465533e5d76c9e0a8aa334545ccc7
Stepan <pirogov1923@mail.ru>
2023-11-14 17:33:05 +0500

    исправил работу правила required для файлов

--- myClss/classes/Validator.php
+++ myClss/classes/Validator.php
@@ -14,6 +14,8 @@ class Validator
         'hasUncap',
         'hasNum',
         'unique',
+        'image',
+        'size',
     ];
     private array $errors_list = [
         'required' => 'Поле является обязательным',
@@ -25,6 +27,8 @@ class Validator
         'hasUncap' => 'Поле должно иметь хотя бы 1 строчную букву',
         'hasNum' => 'Поле должно иметь хотя бы 1 цифру',
         'unique' => 'Пользователь с таким значением поля уже есть',
+        'image' => 'Файл неверного формата',
+        'size' => 'Файл слишком большой',
     ];
 
     public function validate(array $data, array $rules): static
@@ -49,18 +53,18 @@ class Validator
         }
     }
 
-    private function checkRule(string $key, mixed $data, string $rule, $n = null): void
+    private function checkRule(string $key, mixed $data, string $rule, $param = null): void
     {
         if (in_array($rule, $this->rules_list)) {
-            if (!$this->$rule($key, $data, $n)) {
-                $this->addError($key, $rule, $n);
+            if (!$this->$rule($key, $data, $param)) {
+                $this->addError($key, $rule, $param);
             }
         }
     }
 
-    private function addError(string $key, string $rule, string $n = null): void
+    private function addError(string $key, string $rule, string $param = null): void
     {
-        $this->errors[$key][] = str_replace(":number:", $n, $this->errors_list[$rule]);
+        $this->errors[$key][] = str_replace(":number:", $param, $this->errors_list[$rule]);
     }
 
     public function hasErrors(): bool
@@ -73,56 +77,68 @@ class Validator
         return $this->errors;
     }
 
-    private function required($key, $data, $n): bool
+    private function required($key, $data, $param): bool
     {
         return !empty($data);
     }
 
-    private function min($key, $data, $n): bool
+    private function min($key, $data, $param): bool
     {
-        return mb_strlen($data, 'UTF-8') >= $n;
+        return mb_strlen($data, 'UTF-8') >= $param;
     }
 
-    private function max($key, $data, $n): bool
+    private function max($key, $data, $param): bool
     {
-        return mb_strlen($data, 'UTF-8') <= $n;
+        return mb_strlen($data, 'UTF-8') <= $param;
     }
 
-    private function number($key, $data, $n): bool
+    private function number($key, $data, $param): bool
     {
         return is_numeric($data);
     }
 
-    private function string($key, $data, $n): bool
+    private function string($key, $data, $param): bool
     {
         return is_string($data);
     }
 
-    private function hasCap($key, $data, $n): bool
+    private function hasCap($key, $data, $param): bool
     {
         preg_match('/[A-Z]+/', $data, $matches);
         return $matches != [];
     }
 
-    private function hasUncap($key, $data, $n): bool
+    private function hasUncap($key, $data, $param): bool
     {
         preg_match('/[a-z]+/', $data, $matches);
         return $matches != [];
     }
 
-    private function hasNum($key, $data, $n): bool
+    private function hasNum($key, $data, $param): bool
     {
         preg_match('/\d+/', $data, $matches);
         return $matches != [];
     }
 
-    private function unique($key, $data, $n): bool
+    private function unique($key, $data, $param): bool
     {
         /**
          * @var Db $db
          */
         $db = db();
-        $res = $db->rawSql("SELECT * FROM $n WHERE $key = ?", [$data])->findAll();
+        $res = $db->rawSql("SELECT * FROM $param WHERE $key = ?", [$data])->findAll();
         return $res == [];
+    }
+
+    private function image($key, $data, $param): bool
+    {
+        $types = explode('|', $param);
+        $types = array_map(fn ($value) => $value = 'image/' . $value, $types);
+        return in_array($data['type'], $types);
+    }
+
+    private function size($key, $data, $param): bool
+    {
+        return $data['size'] <= $param * 1024 * 1024;
     }
 }
